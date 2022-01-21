aad7393f79d70c88fd106d3103915d77dff50970
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-01-21 20:35:35 +0100

    Finalize random agent

--- src/random_agent.py
+++ src/random_agent.py
@@ -1,9 +1,13 @@
+from stable_baselines3.common.vec_env import DummyVecEnv
+import numpy as np
+
+
 class RandomAgent:
-    def __init__(self, env, **kwargs):
+    def __init__(self, env: DummyVecEnv, **kwargs):
         self.env = env
 
-    def learn(self, **kwargs):
+    def learn(self, **kwargs) -> None:
         pass
 
-    def predict(self, _state):
+    def predict(self, _state: np.ndarray, **kwargs) -> np.ndarray:
         return self.env.action_space.sample(), None
