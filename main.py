8da793d5bd79860569661e3d06dd35ce6584f718
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-01-21 21:47:23 +0100

    Use code from amhe project

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import sys
+from typing import Callable, Type, Union
+
 import gym_anytrading
 import gym
 import numpy as np
@@ -10,8 +14,22 @@ from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 from src.random_agent import RandomAgent
+from src.log_utils import captured_output
 
 # from src.stocks_env_custom import StocksEnvCustom
+
+
+def _create_dirs(*paths: str):
+    for path in paths:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+            
+
+MODEL_TYPE = Type[Union[A2C, PPO, RandomAgent]]
+
+
+def _str_to_class(classname: str) -> MODEL_TYPE:
+    return getattr(sys.modules[__name__], classname)
 
 
 def train_test_split(df: pd.DataFrame, split_point: float = 0.8) -> (pd.DataFrame, pd.DataFrame):
@@ -21,66 +39,98 @@ def train_test_split(df: pd.DataFrame, split_point: float = 0.8) -> (pd.DataFram
     return df_train, df_test
 
 
+def _test_loop(model: MODEL_TYPE, env: gym.Env) -> float:
+    obs = env.reset()
+    total_reward = 0.0
+    done = False
+    while not done:
+        action, _ = model.predict(obs)
+        observation, reward, done, _ = env.step(action)
+        total_reward += reward
+    return total_reward
+
+
+def run_quantstats(env: gym.Env, test_df: pd.DataFrame, window_size: int, output_path: str) -> None:
+    net_worth = pd.Series(env.history['total_profit'], index=test_df.index[window_size + 1:])
+    returns = net_worth.pct_change().iloc[1:]
+    qs.reports.full(returns)
+    qs.reports.html(returns, output=output_path)
+
+
 def main():
     # total_timesteps = 1_000_000
-    total_timesteps = 1_000
-
-    models = {
-        'RandomAgent': lambda verbose: RandomAgent(env=DummyVecEnv([env_maker]), verbose=verbose),
-        'A2C': lambda verbose: A2C(policy='MlpPolicy', env=DummyVecEnv([env_maker]), verbose=verbose),
-        'PPO': lambda verbose: PPO(policy='MlpPolicy', env=DummyVecEnv([env_maker]), verbose=verbose),
-    }
+    total_timesteps = 100
 
     df = gym_anytrading.datasets.STOCKS_GOOGL.copy()
+    train, test = train_test_split(df)
 
     window_size = 10
-    start_index = window_size
-    end_index = len(df)
 
-    env_maker = lambda: gym.make(
-        'stocks-v0',
-        df=df,
-        window_size=window_size,
-        frame_bound=(start_index, end_index)
-    )
+    def env_maker(df: pd.DataFrame, window_size: int) -> Callable[[], gym.Env]:
+        start_index = window_size
+        end_index = len(train)
+        return lambda: gym.make( 'stocks-v0',
+            df=df,
+            window_size=window_size,
+            frame_bound=(start_index, end_index)
+        )
+
+    models = {
+        'RandomAgent': lambda verbose: RandomAgent(env=DummyVecEnv([env_maker(train, window_size)]), verbose=verbose),
+        'A2C': lambda verbose: A2C(policy='MlpPolicy', env=DummyVecEnv([env_maker(train, window_size)]), verbose=verbose),
+        'PPO': lambda verbose: PPO(policy='MlpPolicy', env=DummyVecEnv([env_maker(train, window_size)]), verbose=verbose),
+    }
 
     # env_maker = lambda: StocksEnvCustom(df=df, window_size=window_size, frame_bound=(start_index, end_index))
 
     runs = 3
+    # variable window size?
+    # variable discount_factor?
 
+    data_dir = '.data'
+    logs_dir = f'{data_dir}/logs'
+    models_dir = f'{data_dir}/models'
+    quantstats_dir = f'{data_dir}/quantstats'
+    _create_dirs(logs_dir, models_dir, quantstats_dir)
+    
     for model_name, model_fn in models.items():
         for run in range(runs):
-            print(f'{model_name} run {run}')
-            model = model_fn(verbose=1)
-            model.learn(total_timesteps=total_timesteps)
+            model_path = f'{models_dir}/{model_name}_{run}.zip'
+            print(f'Training {model_name} run {run}')
+            if os.path.exists(model_path):
+                print(f'Model {model_path} already exists, skipping')
+                continue
 
-    env = DummyVecEnv([env_maker])
-    env = env_maker()
-    observation = env.reset()
+            with captured_output() as (out, _):
+                model = model_fn(verbose=1)
+                model.learn(total_timesteps=total_timesteps)
 
-    while True:
-        observation = observation[np.newaxis, ...]
-
-        # action = env.action_space.sample()
-        action, _states = model.predict(observation)
-        observation, reward, done, info = env.step(action)
-
-        # env.render()
-        if done:
-            print("info:", info)
-            break
-
-    plt.figure(figsize=(16, 6))
-    env.render_all()
-    plt.show()
+            log_path = f'{logs_dir}/{model_name}_{run}.log'
+            with open(log_path, 'w') as f:
+                f.write(out.getvalue())
+            model.save(model_path)
 
     qs.extend_pandas()
 
-    net_worth = pd.Series(env.history['total_profit'], index=df.index[start_index + 1:end_index])
-    returns = net_worth.pct_change().iloc[1:]
+    for model_name, _ in models.items():
+        # if model_name == 'RandomAgent':
+        #     continue
+        env = env_maker(test, window_size)()
 
-    qs.reports.full(returns)
-    qs.reports.html(returns, output='a2c_quantstats.html')
+        returns = []
+        for run in range(runs):
+            model_path = f'{models_dir}/{model_name}_0.zip'
+            model = _str_to_class(model_name).load(model_path)
+            return_ = _test_loop(model, env)
+            returns.append(return_)
+            quantstats_output_path = f'{quantstats_dir}/{model_name}_{run}.html'
+            run_quantstats(env, test, window_size, quantstats_output_path)
+        print(f'{model_name} returns (on avg): {sum(returns) / runs}')
+
+
+    # plt.figure(figsize=(16, 6))
+    # env.render_all()
+    # plt.show()
 
 
 if __name__ == '__main__':
